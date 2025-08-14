dab59340120f9466ca8a1e27f09b78ae302b21fc
Rolf Morel <rolf.morel@intel.com>
2025-08-14 14:48:57 -0700

    Minor fixes

--- ingress/mlir-gen/install-virtualenv.sh
+++ ingress/mlir-gen/install-virtualenv.sh
@@ -9,7 +9,5 @@ python -m uv venv mlir-gen-venv --python 3.13
 
 source mlir-gen-venv/bin/activate
 
-EXTRA_INDEX_URL="--extra-index-url https://github.com/makslevental/mlir-wheels/releases/expanded_assets/latest"
-
 echo "Installing mlir-python-bindings and numpy"
 uv pip install numpy mlir-python-bindings -f https://makslevental.github.io/wheels
