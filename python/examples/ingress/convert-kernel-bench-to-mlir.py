78c52e4a2e9dfa5d4aff16878823af35730bcb36
Rolf Morel <rolf.morel@intel.com>
2025-11-26 15:09:32 -0800

    Try to get CI to not kill parallel processes

--- python/examples/ingress/convert-kernel-bench-to-mlir.py
+++ python/examples/ingress/convert-kernel-bench-to-mlir.py
@@ -194,4 +194,4 @@ def process_task(task: KernelConversionTask):
 
 
 print("Output directory:", kernels_as_mlir_folder)
-ProcessPoolExecutor().map(process_task, tasks())
+ProcessPoolExecutor(8).map(process_task, tasks())
