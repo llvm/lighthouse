9bbff3075ac36d8ddc09d8f39b852811641b470b
Schlimbach, Frank <frank.schlimbach@intel.com>
2025-12-16 08:36:47 -0800

    making importing torch and dependent funcs optional

--- lighthouse/utils/__init__.py
+++ lighthouse/utils/__init__.py
@@ -4,9 +4,6 @@ from .runtime_args import (
     get_packed_arg,
     memref_to_ctype,
     memrefs_to_packed_args,
-    torch_to_memref,
-    torch_to_packed_args,
-    mlir_type_to_torch_dtype,
 )
 
 __all__ = [
@@ -17,3 +14,20 @@ __all__ = [
     "torch_to_memref",
     "torch_to_packed_args",
 ]
+
+try:
+    from .runtime_args import (
+        mlir_type_to_torch_dtype,
+        torch_to_memref,
+        torch_to_packed_args,
+    )
+
+    __all__.extend(
+        [
+            "mlir_type_to_torch_dtype",
+            "torch_to_memref",
+            "torch_to_packed_args",
+        ]
+    )
+except:
+    pass
