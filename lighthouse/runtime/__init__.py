a8d23b07167c9692c353a100a76be8a430707d64
Adam Siemieniuk <adam.siemieniuk@intel.com>
2026-01-15 10:52:19 +0100

    use lazy loading for runtime module

--- lighthouse/runtime/__init__.py
+++ lighthouse/runtime/__init__.py
@@ -1,5 +1,18 @@
-from . import torch
+__all__ = ["torch"]
 
-__all__ = [
-    "torch",
-]
+
+import sys
+import importlib
+
+
+def __getattr__(name):
+    """Enable lazy loading of submodules."""
+
+    if name in __all__:
+        # Import the submodule and cache it on the current module. That is,
+        # upon the next access __getattr__ will not be called.
+        submodule = importlib.import_module(__name__ + "." + name)
+        mod = sys.modules[__name__]
+        setattr(mod, name, submodule)
+        return submodule
+    raise AttributeError(f"module '{__name__}' has no attribute '{name}'")
