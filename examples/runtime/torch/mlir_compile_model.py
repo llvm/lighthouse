28d39271c457792aded44e7922bb8b5accf53477
Adam Siemieniuk <adam.siemieniuk@intel.com>
2026-01-15 10:33:48 +0100

    Use model to refer to torch + remove getters

--- examples/runtime/torch/mlir_compile_model.py
+++ examples/runtime/torch/mlir_compile_model.py
@@ -10,12 +10,12 @@ from mlir.passmanager import PassManager
 
 import lighthouse.runtime as lh_runtime
 import lighthouse.utils as lh_utils
-from lighthouse.runtime.torch.jit import JITModule
+from lighthouse.runtime.torch.jit import JITModel
 
 
-def lower_to_llvm(module: ir.Module, ctx: ir.Context) -> ir.Module:
+def lower_to_llvm(module: ir.Module) -> ir.Module:
     """
-    Lower MLIR ops with the module to MLIR LLVM IR dialect.
+    Lower MLIR ops within the module to MLIR LLVM IR dialect.
 
     A PyTorch models are expected to be imported as Linalg ops using tensors.
     This compilation function preprocesses function signature and applies
@@ -23,11 +23,10 @@ def lower_to_llvm(module: ir.Module, ctx: ir.Context) -> ir.Module:
 
     Args:
         module: MLIR module coming from PyTorch importer.
-        ctx: MLIR context
     Returns:
         ir.Module: MLIR module with lowered IR
     """
-    pm = PassManager("builtin.module", ctx)
+    pm = PassManager("builtin.module", module.context)
 
     # Preprocess.
     # Use standard C interface wrappers for functions.
@@ -84,16 +83,16 @@ def jit_decorator_model_class():
             return self.net(x)
 
     # Construct a model object.
-    # Instead of PyTorch module object, a JITModule object is returned.
+    # Instead of PyTorch model object, a JITModel object is returned.
     #
     # The positional and keyword arguments are passes to the model's
     # constructor.
     input_size = 16
     output_size = 8
-    jit_model: JITModule = Model(input_size, out_features=output_size)
+    jit_model: JITModel = Model(input_size, out_features=output_size)
 
-    # The underlying PyTorch model can be accessed through a getter.
-    torch_model = jit_model.get_module()
+    # The underlying PyTorch model can still be accessed.
+    torch_model = jit_model.model
 
     # Compute reference result through the original model.
     # This call executes directly through PyTorch.
@@ -120,9 +119,9 @@ def jit_decorator_model_class():
     # However, the applied MLIR compilation function modifies the resulting
     # jitted function signature.
     # Thus, reference PyTorch inputs are provided through the extra
-    # `module_args` argument.
+    # `model_args` argument.
     out = torch.empty_like(out_ref)
-    jit_model(input, out, module_args=(input,))
+    jit_model(input, out, model_args=(input,))
     is_match = torch.allclose(out_ref, out, rtol=0.01, atol=0.01)
 
     # CHECK: Tensor args - result match: True
@@ -140,7 +139,7 @@ def jit_decorator_model_class():
     # function signature highly depends on the compilation function.
     out = torch.empty_like(out_ref)
     packed_args = lh_utils.torch.to_packed_args([input, out])
-    jit_model(packed_args, module_args=(input,))
+    jit_model(packed_args, model_args=(input,))
     is_match = torch.allclose(out_ref, out, rtol=0.01, atol=0.01)
 
     # CHECK: Packed args - result match: True
@@ -168,11 +167,11 @@ def jit_func_model_object():
 
     # JIT-compile the created PyTorch model object using
     # MLIR function-style JIT API.
-    jit_model: JITModule = lh_runtime.torch.jit(lower_to_llvm, torch_model)
+    jit_model: JITModel = lh_runtime.torch.jit(lower_to_llvm, torch_model)
 
     # Call directly with PyTorch tensors.
     out = torch.empty_like(out_ref)
-    jit_model(a, b, out, module_args=(a, b))
+    jit_model(a, b, out, model_args=(a, b))
     is_match = torch.allclose(out_ref, out, rtol=0.01, atol=0.01)
 
     # CHECK: JIT model object - result match: True
--- lighthouse/runtime/torch/jit.py
+++ lighthouse/runtime/torch/jit.py
@@ -13,88 +13,70 @@ from lighthouse.ingress.torch import import_from_model
 from lighthouse import utils as lh_utils
 
 
-class JITModule:
+class JITModel:
     def __init__(
         self,
-        fn_compile_mlir: Callable[[ir.Module, ir.Context], ir.Module],
-        module: nn.Module,
+        fn_compile_mlir: Callable[[ir.Module], ir.Module],
+        model: nn.Module,
         dialect: OutputType | str = OutputType.LINALG_ON_TENSORS,
         ir_context: ir.Context | None = None,
         shared_libs: Sequence[str] = [],
     ):
         """
-        Initialize the JITModule object.
+        Initialize the JITModel object.
         Typically called from the `@lighthouse.runtime.torch.jit` decorator.
 
         Args:
             fn_compile_mlir: Function to lower imported MLIR to LLVM IR dialect.
-            module: PyTorch module to be compiled through MLIR.
-            dialect: The target dialect for MLIR IR imported from PyTorch module.
+            model: PyTorch model to be compiled through MLIR.
+            dialect: The target dialect for MLIR IR imported from PyTorch model.
             ir_context: An optional MLIR context to use for compilation.
                 If not provided, a new default context is created.
             shared_libs: Paths to external runtime libraries used to execute
                 compiled MLIR function.
         """
         self.fn_compile = fn_compile_mlir
-        self.module = module
+        self.model = model
         self.dialect = OutputType.get(dialect)
-        self.ctx = ir_context
-        if self.ctx is None:
-            self.ctx = ir.Context()
+        self.ctx = ir_context if ir_context is not None else ir.Context()
         self.shared_libs = shared_libs
-
-    def get_module(self) -> nn.Module:
-        """Return initialized PyTorch module."""
-        return self.module
-
-    def get_dialect(self) -> OutputType:
-        """Return target MLIR dialect."""
-        return self.dialect
-
-    def get_context(self) -> ir.Context:
-        """Return MLIR context used by the jitted module."""
-        return self.ctx
-
-    def get_shared_libs(self) -> Sequence[str]:
-        """Return runtime shared libraries."""
-        return self.shared_libs
 
     def __call__(
         self,
         *args: Sequence[torch.Tensor] | object,
-        module_args: Iterable | None = None,
+        model_args: Iterable | None = None,
         **kwargs,
     ) -> Any:
         """
-        Jit the PyTorch module and call the MLIR function.
+        Jit the PyTorch model and call the MLIR function.
 
         Args:
             args: The positional arguments to pass the MLIR function.
                 If all arguments are PyTorch tensors, then they are converted
                 to packed C-type arguments before passing to the MLIR function.
                 Otherwise, `args` are passed directly as is.
-            module_args: The optional positional arguments to the Pytorch module
+            model_args: The optional positional arguments to the Pytorch model
                 required to jit into MLIR.
                 If not provided, `args` are used instead.
-            kwargs: The keyword arguments to the PyTorch module required to jit
+            kwargs: The keyword arguments to the PyTorch model required to jit
                 into MLIR.
 
         Returns:
             Any: The result of the MLIR function call.
         """
 
-        if module_args is None:
-            module_args = args
+        if model_args is None:
+            model_args = args
 
         # TODO: Add caching.
         mlir_mod = import_from_model(
-            self.module,
-            sample_args=module_args,
+            self.model,
+            sample_args=model_args,
             sample_kwargs=kwargs,
             dialect=self.dialect,
             ir_context=self.ctx,
         )
-        mlir_mod = self.fn_compile(mlir_mod, self.ctx)
+        mlir_mod = self.fn_compile(mlir_mod)
 
         eng = ExecutionEngine(mlir_mod, opt_level=3, shared_libs=self.shared_libs)
         eng.initialize()
@@ -108,61 +90,61 @@ class JITModule:
 
 
 def jit(
-    fn_compile: Callable[[ir.Module, ir.Context], ir.Module],
-    module: type[nn.Module] | nn.Module | None = None,
+    fn_compile: Callable[[ir.Module], ir.Module],
+    model: type[nn.Module] | nn.Module | None = None,
     *,
     dialect: OutputType | str = OutputType.LINALG_ON_TENSORS,
     ir_context: ir.Context | None = None,
     shared_libs: Sequence[str] = [],
     **kwargs,
-) -> JITModule | Callable:
+) -> JITModel | Callable:
     """
-    Decorator for JIT-compiling a PyTorch module using MLIR.
+    Decorator for JIT-compiling a PyTorch model using MLIR.
 
-    When a PyTorch module class is passed, the module must be initialized first.
-    Only calls to instantiated module objects are JIT-compiled.
+    When a PyTorch model class is passed, the model must be initialized first.
+    Only calls to instantiated model objects are JIT-compiled.
 
     When a jitted MLIR function is called, input arguments are implicitly converted
     into packed C-type arguments if all inputs are PyTorch tensors.
     Otherwise, the input arguments are directly passed to the jitted function.
 
     The jitted function signature depends on the provided MLIR compilation function
-    `fn_compile` and may differ from the original PyTorch module call signature.
+    `fn_compile` and may differ from the original PyTorch model call signature.
 
     Args:
         fn_compile: Function to compile imported MLIR to LLVM IR dialect.
-            The function accepts an MLIR module and context, and returns
-            an MLIR module with transformed IR.
-        module: PyTorch module to be compiled with MLIR.
+            The function accepts an MLIR module, and returns an MLIR module with
+            transformed IR.
+        model: PyTorch model to be compiled with MLIR.
             If a class or None, a decorator is returned.
-        dialect: The target dialect for MLIR IR imported from PyTorch module.
+        dialect: The target dialect for MLIR IR imported from PyTorch model.
         ir_context: An optional MLIR context to use for compilation.
         shared_libs: Paths to external runtime libraries used to execute
             compiled MLIR function.
-        kwargs: The keyword arguments for the PyTorch module constructor.
+        kwargs: The keyword arguments for the PyTorch model constructor.
 
     Returns:
-        object: A JITModule object or a partially bound decorator.
+        object: A JITModel object or a partially bound decorator.
     """
 
     def class_decorator(
-        cls_module: type[nn.Module],
+        cls_model: type[nn.Module],
         *args,
         dialect: OutputType | str = OutputType.LINALG_ON_TENSORS,
         ir_context: ir.Context | None = None,
         shared_libs: Sequence[str] = [],
         **kwargs,
-    ) -> JITModule:
-        module = cls_module(*args, **kwargs)
-        return JITModule(
+    ) -> JITModel:
+        model = cls_model(*args, **kwargs)
+        return JITModel(
             fn_compile,
-            module,
+            model,
             dialect=dialect,
             ir_context=ir_context,
             shared_libs=shared_libs,
         )
 
-    if module is None:
+    if model is None:
         # Return a partial decorator with bound compilation function.
         return functools.partial(
             jit,
@@ -172,22 +154,22 @@ def jit(
             shared_libs=shared_libs,
             **kwargs,
         )
-    if inspect.isclass(module):
+    if inspect.isclass(model):
         # Return a class decorator which accepts further arguments to
-        # first construct a PyTorch module object before creating
-        # a JITModule object.
+        # first construct a PyTorch model object before creating
+        # a JITModel object.
         return functools.partial(
             class_decorator,
-            module,
+            model,
             dialect=dialect,
             ir_context=ir_context,
             shared_libs=shared_libs,
             **kwargs,
         )
-    # Directly create a JITModule object from an instantiated PyTorch module.
-    return JITModule(
+    # Directly create a JITModel object from an instantiated PyTorch model.
+    return JITModel(
         fn_compile,
-        module,
+        model,
         dialect=dialect,
         ir_context=ir_context,
         shared_libs=shared_libs,
