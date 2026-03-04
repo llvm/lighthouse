a54ea471a11b936f0fcf43e8f38146de1b19f2f0
Renato Golin <rengolin@systemcall.eu>
2026-03-04 22:53:22 +0000

    [infra] Search for FileCheck if available
    To avoid having to remember to set the environment flag. If FileCheck isn't one of the standard ones, symbolic links from user's home bin work too, or even the environment variable still works.

--- lit.cfg.py
+++ lit.cfg.py
@@ -1,11 +1,37 @@
 import os
 import importlib.util
+import shutil
 
 import lit.formats
 from lit.TestingConfig import TestingConfig
 
 # Imagine that, all your variables defined and with type information!
 assert isinstance(config := eval("config"), TestingConfig)
+
+
+def find_filecheck() -> str:
+    """Find the full path of the newest FileCheck in the system."""
+    # If FileCheck is available in path, use it.
+    path = shutil.which("FileCheck")
+    if path:
+        return path
+    # If environment variable is set, use it.
+    if filecheck_path := os.environ.get("FILECHECK"):
+        if os.path.isfile(filecheck_path) and os.access(filecheck_path, os.X_OK):
+            return filecheck_path
+    # Otherwise, search for FileCheck in the system and return the newest one.
+    version = 21  # Latest version
+    while version > 0:
+        path = shutil.which("FileCheck-{}".format(version))
+        if path:
+            return path
+        version -= 1
+    # If not found, raise an error.
+    raise FileNotFoundError(
+        "FileCheck not found in the system. Please install LLVM to get FileCheck or \
+         set the FILECHECK environment variable to point to the FileCheck executable."
+    )
+
 
 project_root = os.path.dirname(__file__)
 
@@ -18,8 +44,7 @@ config.substitutions.append(("%CACHE", project_root + "/cache"))
 config.substitutions.append(("%VIRTUAL_ENV", os.environ.get("VIRTUAL_ENV", "")))
 python = os.environ.get("PYTHON", "python")
 config.substitutions.append(("%PYTHON", python))
-if filecheck_path := os.environ.get("FILECHECK"):
-    config.substitutions.append(("FileCheck", filecheck_path))
+config.substitutions.append(("FileCheck", find_filecheck()))
 
 for pkg in ["torch", "mpi4py", "mpich", "impi-rt"]:
     if importlib.util.find_spec(pkg):
