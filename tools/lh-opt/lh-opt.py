e7507aed1d23a0419683d1ad4ba65d7373e02b9e
Renato Golin <rengolin@systemcall.eu>
2026-03-12 13:51:24 +0000

    [tools] Introduce lh-opt
    This is a new testing tool that uses the new lighthouse module to create
    and drive pipelines of both passes and transforms onto a payload module.
    For now there's only one test using passes, because applying transforms
    is more complex. I'll add support for this in lh-opt in a follow-up PR.

---
+++ tools/lh-opt/lh-opt.py
@@ -0,0 +1,61 @@
+#! /usr/bin/env python
+
+import argparse
+
+from mlir import ir
+from lighthouse.pipeline.opt import Driver
+
+
+def import_payload(path: str) -> ir.Module:
+    """Import an MLIR text file into the payload module"""
+    with ir.Context():
+        with open(path, "r") as f:
+            return ir.Module.parse(f.read())
+
+
+def create_driver(module: ir.Module, stages: list[str]) -> Driver:
+    """Create the driver's pipeline by selecting the passes that will run"""
+    driver = Driver(module)
+    if not stages:
+        # Add all stages if no stages are specified.
+        driver.bufferize()
+        driver.mlir_lowering()
+        driver.llvm_lowering()
+        driver.cleanup()
+    else:
+        for t in stages:
+            if t == "bufferize":
+                driver.bufferize()
+            elif t == "mlir_lowering":
+                driver.mlir_lowering()
+            elif t == "llvm_lowering":
+                driver.llvm_lowering()
+            elif t == "cleanup":
+                driver.cleanup()
+            else:
+                raise ValueError(f"Unsupported transformation: {t}")
+
+    return driver
+
+
+if __name__ == "__main__":
+    Parser = argparse.ArgumentParser(description="Lighthouse Optimization Pipeline")
+    Parser.add_argument(
+        "payload_module", type=str, help="Path to the payload MLIR module to optimize."
+    )
+    Parser.add_argument(
+        "--stage",
+        action="append",
+        help="List of transformations to apply to the input module. Supported transformations: bufferize, lower_to_llvm, mlir_lowering, cleanup.",
+    )
+    args = Parser.parse_args()
+
+    # Load the input module.
+    input_module = import_payload(args.payload_module)
+
+    # Create the driver and run the transformations.
+    driver = create_driver(input_module, args.stage)
+
+    # Run the pipeline and get the optimized module.
+    optimized_module = driver.run()
+    print(optimized_module)
