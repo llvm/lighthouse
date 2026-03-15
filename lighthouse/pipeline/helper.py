05c4b4167d9fb9720776e1e0c0185ada21687634
Renato Golin <rengolin@systemcall.eu>
2026-03-15 17:49:08 +0000

    major refactor, merge opt and driver

--- lighthouse/pipeline/helper.py
+++ lighthouse/pipeline/helper.py
@@ -1,17 +1,31 @@
+import os
+
 from mlir import ir
 from mlir.dialects import transform
 from mlir.dialects.transform import structured
 
 
-# Utility function to add a bundle of passes to a Transform Schedule. This can be used to easily add a predefined set of passes to a pipeline.
+def import_mlir_module(path: str, context: ir.Context) -> ir.Module:
+    """Import an MLIR text file into an MLIR module"""
+    if path is None:
+        raise ValueError("Path to the module must be provided.")
+    if not os.path.exists(path):
+        raise ValueError(f"Path to the module does not exist: {path}")
+    with open(path, "r") as f:
+        return ir.Module.parse(f.read(), context=context)
+
+
 def apply_registered_pass(*args, **kwargs):
+    """Utility function to add a bundle of passes to a Transform Schedule"""
     return transform.apply_registered_pass(transform.AnyOpType.get(), *args, **kwargs)
 
 
 def match(*args, **kwargs):
+    """Matches a pattern to AnyType"""
     return structured.structured_match(transform.AnyOpType.get(), *args, **kwargs)
 
 
 def canonicalize(op):
+    """Runs canonicalization patterns on the given operation"""
     with ir.InsertionPoint(transform.apply_patterns(op).patterns):
         transform.apply_patterns_canonicalization()
