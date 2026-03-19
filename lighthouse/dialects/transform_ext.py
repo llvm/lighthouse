d395035ea5c5dd0538f05f9a21a2ef321587afc7
Rolf Morel <rolf.morel@intel.com>
2026-03-19 08:52:43 -0700

    Get rid of rewrite pattern and just make into a normal transform op

--- lighthouse/dialects/transform_ext.py
+++ lighthouse/dialects/transform_ext.py
@@ -1,5 +1,8 @@
-from mlir import rewrite, ir
-from mlir.dialects import ext, transform
+from mlir import ir
+from mlir.dialects import ext, transform, func, arith, scf, memref
+from mlir.dialects.transform import DiagnosedSilenceableFailure
+
+from lighthouse.utils.mlir import func_cif
 
 
 def register_and_load(context=None):
@@ -15,46 +18,112 @@ class TransformExtensionDialect(ext.Dialect, name="transform_ext"):
                 op_cls.attach_interface_impls()
 
 
-class PopulatePatternOp(TransformExtensionDialect.Operation, name="populate_pattern"):
-    """An operation to populate a pattern set with a specific pattern.
+class WrapInBenchingFuncOp(
+    TransformExtensionDialect.Operation, name="wrap_in_benching_func"
+):
+    """Create a function that calls `target` function in a benchmarking loop.
 
-    To be used in the region of `transform.apply_patterns`."""
+    The new function has the same arguments as `target` plus three additional ones:
+    - A memref to store the timing results (one element per iteration).
+    - The number of timed iterations.
+    - The number of warmup iterations.
+    """
 
-    pattern_name: ir.StringAttr
-    op_kind: ir.StringAttr
-    priority: ir.IntegerAttr
-
-    # A mapping from pattern names to their corresponding rewrite functions.
-    # This should be populated by the users of this operation. In effect serves
-    # as a registry for rewrite patterns that can be applied by this operation.
-    name_to_rewrite_pattern = {}
+    target: ext.Operand[transform.AnyOpType]
+    bench_func: ext.Result[transform.AnyOpType[()]]
 
     @classmethod
     def attach_interface_impls(cls, context=None):
-        cls.PatternDescriptorOpInterfaceModel.attach(
-            cls.OPERATION_NAME, context=context
-        )
+        cls.TransformOpInterfaceModel.attach(cls.OPERATION_NAME, context=context)
+        cls.MemoryEffectsOpInterfaceModel.attach(cls.OPERATION_NAME, context=context)
 
-    class PatternDescriptorOpInterfaceModel(transform.PatternDescriptorOpInterface):
+    @staticmethod
+    def wrap_in_benching_func(target: func.FuncOp, bench_name: str):
+        """Create a function that calls `target` in a benchmarking loop.
+
+        Each call to `target` is timed separately, and the times (in seconds)
+        are stored in a memref that is passed as an additional argument to the
+        benchmark function. It also takes two additional arguments for the
+        number of runs and warmup iterations.
+        """
+
+        # define rtclock function
+        f64_t = ir.F64Type.get()
+        func.FuncOp("rtclock", ((), (f64_t,)), visibility="private")
+        # emit benchmark function
+        time_memref_t = ir.MemRefType.get((ir.ShapedType.get_dynamic_size(),), f64_t)
+        index_t = ir.IndexType.get()
+        args = target.type.inputs + [time_memref_t, index_t, index_t]
+
+        @func_cif(*args, name=bench_name)
+        def bench(*args):
+            zero = arith.constant(index_t, 0)
+            one = arith.constant(index_t, 1)
+            func_args = list(args[: len(target.type.inputs)])
+            times_memref, num_times, num_warmup = args[-3:]
+            for i in scf.for_(zero, num_warmup, one):
+                # FIXME(upstream): func.call needs to wrap _overridden_ CallOp.
+                func.CallOp(target, func_args)
+                scf.yield_(())
+            # TODO: get `num_times` from the `times_memref`.
+            for i in scf.for_(zero, num_times, one):
+                tic = func.call((f64_t,), "rtclock", ())
+                func.CallOp(target, func_args)
+                toc = func.call((f64_t,), "rtclock", ())
+                time = arith.subf(toc, tic)
+                memref.store(time, times_memref, [i])
+                scf.yield_(())
+
+        return bench.func_op
+
+    class TransformOpInterfaceModel(transform.TransformOpInterface):
         @staticmethod
-        def populate_patterns(
-            op: "PopulatePatternOp",
-            patternset: rewrite.RewritePatternSet,
-        ) -> None:
-            patternset.add(
-                op.op_kind.value,
-                op.name_to_rewrite_pattern[op.pattern_name.value],
-                benefit=op.priority.value,
-            )
+        def apply(
+            op: "WrapInBenchingFuncOp",
+            _rewriter: transform.TransformRewriter,
+            results: transform.TransformResults,
+            state: transform.TransformState,
+        ) -> DiagnosedSilenceableFailure:
+            targets = state.get_payload_ops(op.target)
+            if bench_name_attr := op.attributes.get("bench_name"):
+                bench_name = bench_name_attr.value
+                if len(targets) != 1:
+                    return DiagnosedSilenceableFailure.SilenceableFailure
+            else:
+                bench_name = None
+
+            bench_funcs = []
+            for target in targets:
+                if not isinstance(target, func.FuncOp):
+                    return DiagnosedSilenceableFailure.SilenceableFailure
+
+                with ir.InsertionPoint(target), target.location:
+                    bench_func = WrapInBenchingFuncOp.wrap_in_benching_func(
+                        target, bench_name or f"bench_{target.name.value}"
+                    )
+                    bench_funcs.append(bench_func)
+
+            results.set_ops(op.bench_func, bench_funcs)
+
+            return DiagnosedSilenceableFailure.Success
+
+        @staticmethod
+        def allow_repeated_handle_operands(_op: "WrapInBenchingFuncOp") -> bool:
+            return False
+
+    class MemoryEffectsOpInterfaceModel(ir.MemoryEffectsOpInterface):
+        @staticmethod
+        def get_effects(op: "WrapInBenchingFuncOp", effects):
+            transform.only_reads_handle(op.op_operands, effects)
+            transform.produces_handle(op.results, effects)
+            transform.modifies_payload(effects)
 
 
-def populate_pattern(
-    pattern_name: str, op_kind: str, priority: int
-) -> PopulatePatternOp:
-    """Camelcase constructor for PopulatePatternOp."""
-    priority_attr = ir.IntegerAttr.get(ir.IntegerType.get_signless(32), priority)
-    return PopulatePatternOp(
-        op_kind=ir.StringAttr.get(op_kind),
-        pattern_name=ir.StringAttr.get(pattern_name),
-        priority=priority_attr,
-    )
+def wrap_in_benching_func(
+    target: ir.Value[transform.AnyOpType], bench_name: str | None = None
+) -> ir.Value[transform.AnyOpType]:
+    """snake_case wrapper to create a WrapInBenchingFuncOp."""
+    op = WrapInBenchingFuncOp(target=target)
+    if bench_name is not None:
+        op.attributes["bench_name"] = ir.StringAttr.get(bench_name)
+    return op.bench_func
--- lighthouse/workload/runner.py
+++ lighthouse/workload/runner.py
@@ -5,12 +5,14 @@ Utility functions for running workloads.
 import numpy as np
 import os
 from mlir import ir
-from mlir.dialects import func, arith, scf, memref, transform
+from mlir.dialects import transform
+from mlir.dialects.transform import structured
 from mlir.execution_engine import ExecutionEngine
 from mlir.runtime.np_to_memref import get_ranked_memref_descriptor
-from lighthouse.dialects.transform_ext import PopulatePatternOp, populate_pattern
+
+from lighthouse.dialects import transform_ext
 from lighthouse.schedule.utils import schedule_boilerplate
-from lighthouse.utils.mlir import func_cif, get_mlir_library_path
+from lighthouse.utils.mlir import get_mlir_library_path
 from lighthouse.utils.memref import to_packed_args
 from lighthouse.workload import Workload
 from typing import Optional
@@ -67,75 +69,18 @@ def execute(
                 raise ValueError("Benchmark verification failed.")
 
 
-def bench_wrapper_pattern(funcname: str, benchname=None):
-    """Returns a rewrite pattern that matches a function named `funcname` and clones it
-    as a new function with name given by `benchname` (default: "bench_" + funcname).
-    The new function is a benchmark wrapper that calls the payload function and times it.
-    Every function call is timed separately. Returns the times (seconds) in a memref,
-    which is passed as an additional argument to the benchmark function.
-    It also takes two additional arguments for the number of runs and warmup iterations.
-    """
-    marker = "__bench_wrapped__"
-    if benchname is None:
-        benchname = f"bench_{funcname}"
-
-    def match_and_rewrite(op, rewriter):
-        if op.name.value != funcname:
-            return True  # Failed match, return truthy value
-        if marker in op.attributes:
-            return True  # Already wrapped, skip
-        payload_arguments = op.type.inputs
-
-        with rewriter.ip, op.location:
-            # define rtclock function
-            f64_t = ir.F64Type.get()
-            func.FuncOp("rtclock", ((), (f64_t,)), visibility="private")
-            # emit benchmark function
-            time_memref_t = ir.MemRefType.get(
-                (ir.ShapedType.get_dynamic_size(),), f64_t
-            )
-            index_t = ir.IndexType.get()
-            args = payload_arguments + [time_memref_t, index_t, index_t]
-
-            @func_cif(*args, name=benchname)
-            def bench(*args):
-                index_t = ir.IndexType.get()
-                zero = arith.constant(index_t, 0)
-                one = arith.constant(index_t, 1)
-                for i in scf.for_(zero, args[-1], one):
-                    # FIXME(upstream): func.call is broken for this use case?
-                    func.CallOp(op, list(args[: len(payload_arguments)]))
-                    scf.yield_(())
-                for i in scf.for_(zero, args[-2], one):
-                    tic = func.call((f64_t,), "rtclock", ())
-                    func.CallOp(op, list(args[: len(payload_arguments)]))
-                    toc = func.call((f64_t,), "rtclock", ())
-                    time = arith.subf(toc, tic)
-                    memref.store(time, args[-3], [i])
-                    scf.yield_(())
-
-        # Mark original function as wrapped
-        op.attributes[marker] = ir.UnitAttr.get()
-        return None  # Success
-
-    return match_and_rewrite
-
-
 def get_bench_wrapper_schedule(workload: Workload):
-    PopulatePatternOp.name_to_rewrite_pattern["bench_wrapper"] = bench_wrapper_pattern(
-        workload.payload_function_name,
-        workload.benchmark_function_name,
-    )
-
     with schedule_boilerplate() as (schedule, named_seq):
-        apply_patterns_op = transform.apply_patterns(named_seq.bodyTarget)
-        with ir.InsertionPoint(apply_patterns_op.patterns):
-            populate_pattern(
-                pattern_name="bench_wrapper",
-                op_kind="func.func",
-                priority=1,
-            )
-        transform.yield_([named_seq.bodyTarget])
+        named_func = structured.structured_match(
+            transform.AnyOpType.get(),
+            target=named_seq.bodyTarget,
+            ops={"func.func"},
+            op_attrs={"sym_name": ir.StringAttr.get(workload.payload_function_name)},
+        )
+        bench_func = transform_ext.wrap_in_benching_func(
+            named_func, bench_name=workload.benchmark_function_name
+        )
+        transform.yield_([bench_func])
 
     schedule.body.operations[0].verify()
     return schedule
