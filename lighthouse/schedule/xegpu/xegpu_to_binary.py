728b628a4f65c6ef3b374b9e35e54359f9d3e4ff
Tuomas Karna <tuomas.karna@intel.com>
2026-04-27 10:31:56 +0300

    xegpu_to_binary: fix typo

--- lighthouse/schedule/xegpu/xegpu_to_binary.py
+++ lighthouse/schedule/xegpu/xegpu_to_binary.py
@@ -5,12 +5,12 @@ from ..builders import schedule_boilerplate
 from lighthouse.pipeline.helper import apply_registered_pass
 
 
-def xegpu_to_binary(xegppu_op_level: str = "workgroup") -> ir.Module:
+def xegpu_to_binary(xegpu_op_level: str = "workgroup") -> ir.Module:
     """
     Lower XeGPU dialect to binary using the default upstream pipeline.
 
     Args:
-        xegppu_op_level: Initial XeGPU operation level.
+        xegpu_op_level: Initial XeGPU operation level.
     Returns:
         Schedule
     """
@@ -19,7 +19,7 @@ def xegpu_to_binary(xegppu_op_level: str = "workgroup") -> ir.Module:
         apply_registered_pass(
             target,
             "gpu-lower-to-xevm-pipeline",
-            options={"xegpu-op-level": xegppu_op_level},
+            options={"xegpu-op-level": xegpu_op_level},
         )
 
         transform.yield_()
