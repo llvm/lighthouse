4c290f6f58bb3eba600eb726b9c65991610902b1
Tuomas Karna <tuomas.karna@intel.com>
2026-04-28 12:23:48 +0300

    xegpu_to_binary: add large_register_file option

--- lighthouse/schedule/xegpu/xegpu_to_binary.py
+++ lighthouse/schedule/xegpu/xegpu_to_binary.py
@@ -5,21 +5,29 @@ from ..builders import schedule_boilerplate
 from lighthouse.pipeline.helper import apply_registered_pass
 
 
-def xegpu_to_binary(xegpu_op_level: str = "workgroup") -> ir.Module:
+def xegpu_to_binary(
+    xegpu_op_level: str = "workgroup", large_register_file: bool = True
+) -> ir.Module:
     """
     Lower XeGPU dialect to binary using the default upstream pipeline.
 
     Args:
         xegpu_op_level: Initial XeGPU operation level.
+        large_register_file: Whether to enable large register file.
     Returns:
         Schedule
     """
+    options = {
+        "xegpu-op-level": xegpu_op_level,
+    }
+    if large_register_file:
+        options["igc-cmd-options"] = "-ze-opt-large-register-file"
     with schedule_boilerplate() as (schedule, named_seq):
         target = named_seq.bodyTarget
         apply_registered_pass(
             target,
             "gpu-lower-to-xevm-pipeline",
-            options={"xegpu-op-level": xegpu_op_level},
+            options=options,
         )
 
         transform.yield_()
