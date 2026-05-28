b6efa324ee3c00ae354da5de556fb15f35989072
Tuomas Karna <tuomas.karna@intel.com>
2026-05-28 21:19:34 +0300

    xegpu parameter selector: use dict input and support transposes

--- lighthouse/schedule/xegpu/xegpu_parameter_selector.py
+++ lighthouse/schedule/xegpu/xegpu_parameter_selector.py
@@ -30,12 +30,27 @@ class XeGPUParameterSelector:
         self.gpu_specs = XeGPUSpecs.get(self.device)
         self.matmul_param_db = load_param_database(json_file)
 
-    def get_parameters(self, m: int, n: int, k: int) -> dict:
-        shape = (m, n, k)
-        if shape not in self.matmul_param_db:
+    def get_parameters(
+        self,
+        shape: tuple[int, int, int],
+        transpose_a: bool = False,
+        transpose_b: bool = False,
+        **kwargs,
+    ) -> dict:
+        m, n, k = shape
+        if shape not in self.matmul_param_db or transpose_a or transpose_b:
             try:
                 # Use cost model to generate tile sizes and take first config
-                configs = generate_configs(m, n, k, self.gpu_specs, max_nb_configs=1)
+                configs = generate_configs(
+                    m,
+                    n,
+                    k,
+                    self.gpu_specs,
+                    transpose_a=transpose_a,
+                    transpose_b=transpose_b,
+                    max_nb_configs=1,
+                    verbose=False,
+                )
                 if not configs:
                     raise ValueError(
                         f"Cost model did not return any valid configurations for matmul {shape}."
@@ -45,7 +60,11 @@ class XeGPUParameterSelector:
             except Exception as e:
                 msg = f"Error generating parameters for shape {shape} using cost model: {e}"
                 raise ValueError(msg) from e
-        return self.matmul_param_db[shape]
+        params = self.matmul_param_db[shape]
+        # ensure transpose flags are set
+        params.setdefault("transpose_a", False)
+        params.setdefault("transpose_b", False)
+        return params
 
-    def get_parameters_for_layers(self, shapes: list[tuple[int, int, int]]) -> list:
-        return [self.get_parameters(*shape) for shape in shapes]
+    def get_parameters_for_layers(self, param_list: list[dict]) -> list:
+        return [self.get_parameters(**params) for params in param_list]
